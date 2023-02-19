7740a351c0ee27b11a5a25b6907223c7db3e7cf3
Aglaia Kakoulidou <kakoulidoumaj7@gmail.com>
2023-02-19 10:04:02 +0100

    Add files via upload

---
+++ Python for Everybody/Chapter 4/Exercise 7.py
@@ -0,0 +1,24 @@
+score= input('Please enter your score')
+
+def computegrade(score):
+
+    try:
+
+        if float(score) >=0 and float(score) <=1.0:
+                if float(score) >= 0.9:
+                    print ('A')
+                elif float(score) >= 0.8:
+                    print ('B')
+                elif float(score) >=0.7:
+                    print ('C')
+                elif float(score) >=0.6:
+                    print ('D')
+                else:
+                    print ('F')
+            
+        else:
+            print ("Bad score")
+    except: 
+        print ("please enter a numerical value between 0 and 1")
+
+computegrade(score)
